98fecd6c030665d9b0f9014eb446a10dcc31555c
Luke Cheng <Lukeecheng@gmail.com>
2024-11-25 11:07:36 -0800

    2nd stt migration

--- backend/stt.py
+++ backend/stt.py
@@ -2,8 +2,9 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
-client = OpenAI()
 load_dotenv()
+client = OpenAI()
+
 
 # Get the API key from environment variables
 api_key = os.getenv("OPENAI_API_KEY")
@@ -11,11 +12,11 @@ if not api_key:
     raise ValueError("OPENAI_API_KEY is not set in the environment variables")
 
 
-audio_file= open("/Users/lukecheng/Task-scheduler/frontend/src/audio/recording.wav", "rb")
+audio_file= open("/Users/lukecheng/Mern-scheduler/backend/audio/recording.wav", "rb")
 transcription = client.audio.transcriptions.create(
   model="whisper-1", 
   file=audio_file,
   response_format="text"
 )
-print(transcription.text)
+print(transcription)
 
